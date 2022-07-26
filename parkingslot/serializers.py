125f8fc5c35703d44d3aa9b52dd68d1ea2c793eb
Gabriel Esteves <brielesteves@gmail.com>
2022-07-26 09:18:14 -0300

    add serializers

--- parkingslot/serializers.py
+++ parkingslot/serializers.py
@@ -2,13 +2,13 @@ from rest_framework import serializers
 from .models import ParkingSlot, CarModel
 
 
-class ParkingSlotSerializer(serializers.ModelSerializer):
+class ParkingSlotSerializer(serializers.ModelSerializer): 
     class Meta:
         model = ParkingSlot
         fields = '__all__'
 
 
-class CarModelSerializer(serializers.ModelSerializer):
+class CarModelSerializer(serializers.ModelSerializer): 
     class Meta: 
         model = CarModel
         fields = '__all__'
