abb674440cd3aa0709585b828e93bd844b014996
Gabriel Esteves <brielesteves@gmail.com>
2022-07-26 09:14:51 -0300

    add settings

--- parkingmanage/settings.py
+++ parkingmanage/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^4x&$=h#+*_9gc_=!#jdfej53wtke14wr1_k&193nqd3e+5s7q'
-
+SECRET_KEY = #Your Password
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -31,14 +30,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'parkingslot.apps.ParkingslotConfig',
-    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #libs
+    'rest_framework',
+
+    #apps
+    'parkingslot.apps.ParkingslotConfig',
 ]
 
 MIDDLEWARE = [
